bb179740779a7f2c8cefa58571bc81335473cc94
Taylor-zym <romtance@163.com>
2023-04-04 22:57:40 +0800

    可以展示界面了

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-import codecs
-import os
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-with codecs.open(os.path.join(here, "README.rst"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
-
-VERSION = '0.0.3.8'
+VERSION = '0.0.3.9'
 DESCRIPTION = '灰色系统包，目前只有一个功能，未来会有很多！'
 LONG_DESCRIPTION = '我需要很长的时间去完善，目前只是一个占位，将在未来完善，也许是一年以后啦~，现在我要带上耳机，数学分析'
 
